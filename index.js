aaa3b532557430ca20f65727543bcf68d3bfb5bd
Mati Joseph <crucialniccur@gmail.com>
2025-02-24 07:38:11 +0300

    console.log firstName but expecting an error

--- index.js
+++ index.js
@@ -1,45 +1,42 @@
 // Follow along with the examples here
-function doNothing() {
-    
-}
+function doNothing() {}
 
 function sayHello() {
-    console.log('Hello!');
+  console.log("Hello!");
 }
 
 sayHello();
 
-
 function sayHelloGuadalupe() {
-    console.log('Hello, Guadalupe!');
+  console.log("Hello, Guadalupe!");
 }
 
 sayHelloGuadalupe();
 
 function sayHelloToLiz() {
-    console.log('Hello, Liz!');
+  console.log("Hello, Liz!");
 }
 
 sayHelloToLiz();
 
 function sayHelloToSamip() {
-    console.log('Hello, Samip!');
+  console.log("Hello, Samip!");
 }
 
 sayHelloToSamip();
-
 
 sayHelloGuadalupe();
 sayHelloToLiz();
 sayHelloToSamip();
 
-
 function sayHelloTo(firstName) {
-    console.log(`Hello, ${firstName}!`);
+  console.log(`Hello, ${firstName}!`);
 }
 
-sayHelloTo('Guadalupe');
-sayHelloTo('Jane');
-sayHelloTo('R2-D2');
+sayHelloTo("Guadalupe");
+sayHelloTo("Jane");
+sayHelloTo("R2-D2");
 sayHelloTo(1);
-sayHelloTo('Mati');
+sayHelloTo("Mati");
+
+console.log(firstName);
